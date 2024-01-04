932e71848b3deeaed350678040250f161f1c29ba
Son JunHyung <109019081+sonjh919@users.noreply.github.com>
2024-01-04 09:46:18 +0900

    [level unrated] Title: 각도기, Time: 0.02 ms, Memory: 67.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120829. 각도기/각도기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int angle) {
+        if(angle==180)return 4;
+        if(angle>90)return 3;
+        if(angle==90)return 2;
+        return 1;
+        
+    }
+}
