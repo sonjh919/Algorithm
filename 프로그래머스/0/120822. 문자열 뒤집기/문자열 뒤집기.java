302c84be78633f52f50cb9684f932e8afd002b57
Son JunHyung <109019081+sonjh919@users.noreply.github.com>
2024-02-13 15:38:49 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder sb = new StringBuilder(my_string);
+        sb.reverse();
+        return sb.toString();
+    }
+}
