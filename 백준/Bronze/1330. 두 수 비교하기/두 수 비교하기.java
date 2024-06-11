9138d9f19b63918e58bf74ccff1d5e6d610beb8c
Son JunHyung <109019081+sonjh919@users.noreply.github.com>
2024-06-12 00:52:31 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 140 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+        static int A;
+        static int B;
+
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            A = Integer.parseInt(st.nextToken());
+            B = Integer.parseInt(st.nextToken());
+
+            if(A>B)
+                bw.write(">");
+            else if(A<B)
+                bw.write("<");
+            else
+                bw.write("==");
+
+            bw.flush();
+            bw.close();
+            br.close();
+        }
+    }
