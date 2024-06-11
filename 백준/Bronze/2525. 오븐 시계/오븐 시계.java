5ec13ae635ddbf77ff7c4550dc9a952c9692ae9e
Son JunHyung <109019081+sonjh919@users.noreply.github.com>
2024-06-12 01:16:15 +0900

    [Bronze III] Title: 오븐 시계, Time: 156 ms, Memory: 16324 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+        static int A;
+        static int B;
+        static int C;
+
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            A = Integer.parseInt(st.nextToken());
+            B = Integer.parseInt(st.nextToken());
+
+            C = Integer.parseInt(br.readLine());
+
+            B += C;
+
+            while(B >= 60) {
+                B -= 60;
+                A++;
+                if (A >= 24) {
+                    A -= 24;
+                }
+            }
+
+            bw.write(String.valueOf(A) + " " + String.valueOf(B) + "\n");
+
+            bw.flush();
+            bw.close();
+            br.close();
+        }
+    }
