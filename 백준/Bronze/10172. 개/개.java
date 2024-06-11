192c7e8c7ba1e253590c685d496e983e504c434b
Son JunHyung <109019081+sonjh919@users.noreply.github.com>
2024-06-12 00:37:06 +0900

    [Bronze V] Title: 개, Time: 120 ms, Memory: 14092 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+    public class Main {
+
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+            bw.write("|\\_/|\n"
+                + "|q p|   /}\n"
+                + "( 0 )\"\"\"\\\n"
+                + "|\"^\"`    |\n"
+                + "||_/=\\\\__|");
+
+
+            bw.flush();
+            bw.close();
+            br.close();
+        }
+    }
