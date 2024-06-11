0f5a8f9a4926cb125d9c79cfbd70d460d5463dff
Son JunHyung <109019081+sonjh919@users.noreply.github.com>
2024-06-11 23:56:38 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 128 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        n = Integer.parseInt(br.readLine());
+
+        bw.write(String.valueOf(n-543));
+
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
