ebd991f29ea1f81bf94721a0a9cfcdecc7467f44
Son JunHyung <109019081+sonjh919@users.noreply.github.com>
2024-06-11 21:00:42 +0900

    [Bronze V] Title: ??!, Time: 140 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    static String s;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        s = br.readLine();
+        bw.write(s + "??!");
+        
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
