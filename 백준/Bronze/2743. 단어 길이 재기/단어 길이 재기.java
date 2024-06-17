828e275dfe1eacb56e22b354ffd1fe942e610e15
Son JunHyung <109019081+sonjh919@users.noreply.github.com>
2024-06-17 14:40:26 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    static String S;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        S = br.readLine();
+
+        bw.write(String.valueOf(S.length()));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
