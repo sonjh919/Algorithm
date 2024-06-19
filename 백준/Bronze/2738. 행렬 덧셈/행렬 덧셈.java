04d8c63d7858c320665564e47986c5404649173b
Son JunHyung <109019081+sonjh919@users.noreply.github.com>
2024-06-20 00:21:11 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 228 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N,M;
+    static int[][] map;
+
+    public static void main(String[] args) throws IOException {
+        //given
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        //when
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                bw.write(map[i][j] + Integer.parseInt(st.nextToken())+" ");
+            }
+            bw.write("\n");
+        }
+
+        //then
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+
+}
