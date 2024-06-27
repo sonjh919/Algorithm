3a00677e0a3c78fea9e7c7bb64203381b84369c2
Son JunHyung <109019081+sonjh919@users.noreply.github.com>
2024-06-27 15:31:21 +0900

    [Silver II] Title: 좌표 압축, Time: 2612 ms, Memory: 214420 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        //given
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        //when
+//        List<Integer> list2 = list.stream().distinct().collect(Collectors.toList());
+        Set<Integer> set = new HashSet<>(list);
+        List<Integer> list2 = new ArrayList<>(set);
+        Collections.sort(list2);
+        Object[] sorted = list2.toArray();
+        for (Integer integer : list) {
+//            bw.write(list2.indexOf(integer)+" ");
+            bw.write(Arrays.binarySearch(sorted, integer)+" ");
+        }
+
+        //then
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
