5588c723be9bbff6951bfe1412c71c18ca40f297
Son JunHyung <109019081+sonjh919@users.noreply.github.com>
2024-07-02 00:33:27 +0900

    [Silver III] Title: 분수 합, Time: 144 ms, Memory: 16176 KB -BaekjoonHub

---
+++ 백준/Silver/1735. 분수 합/분수 합.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int C = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+
+        int E = A*D + B*C;
+        int F = B*D;
+        
+        int gcd = (BigInteger.valueOf(E).gcd(BigInteger.valueOf(F))).intValue();
+
+        bw.write(E/gcd + " " + F/gcd);
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
