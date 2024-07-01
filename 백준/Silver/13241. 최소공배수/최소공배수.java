b4673415b3ae33b02b561662acaec4577e49575c
Son JunHyung <109019081+sonjh919@users.noreply.github.com>
2024-07-02 00:25:29 +0900

    [Silver V] Title: 최소공배수, Time: 108 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+
+        Long lcm = (A * B) / (BigInteger.valueOf(A).gcd(BigInteger.valueOf(B))).longValue();
+
+        bw.write(String.valueOf(lcm));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
