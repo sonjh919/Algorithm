0daff3f33b9712f979bd4154e6bd7aff2f03c138
Son JunHyung <109019081+sonjh919@users.noreply.github.com>
2024-07-14 21:01:53 +0900

    [Silver II] Title: 최소 힙, Time: 324 ms, Memory: 26652 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.PriorityQueue;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+
+        int N = Integer.parseInt(br.readLine());
+        int x;
+        for (int i = 0; i < N; i++) {
+            x = Integer.parseInt(br.readLine());
+            if(x==0){
+                bw.write(priorityQueue.isEmpty()?"0\n":priorityQueue.poll() + "\n");
+            }
+            else{
+                priorityQueue.add(x);
+            }
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
