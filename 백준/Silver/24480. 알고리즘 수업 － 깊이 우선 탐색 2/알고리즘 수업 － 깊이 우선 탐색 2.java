43249a5c2ee3f555f3b2a6d61170a6f3e1035d09
Son JunHyung <109019081+sonjh919@users.noreply.github.com>
2024-07-15 21:56:17 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 968 ms, Memory: 94852 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static List<List<Integer>> graph = new ArrayList<>(); // 그래프
+    static boolean[] visited; // 방문했는지
+    static int[] order; // 방문 순서
+    static int cnt = 1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int N; // 정점의 개수
+        int M; // 간선의 수
+        int R; // 시작 정점
+        int u,v;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N + 1];
+        order = new int[N + 1];
+
+        for (int i = 0; i <= N; i++) { // 1부터 N까지, 0은 생략
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            u = Integer.parseInt(st.nextToken());
+            v = Integer.parseInt(st.nextToken());
+
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            graph.get(i).sort(Collections.reverseOrder());
+        }
+
+        DFS(R);
+
+        for (int i = 1; i <= N; i++) {
+            bw.write(order[i]+ "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private static void DFS(int node) {
+        visited[node] = true;
+        order[node] = cnt++;
+
+        for(int next : graph.get(node)){
+            if(!visited[next]){
+                DFS(next);
+            }
+        }
+    }
+
+}
