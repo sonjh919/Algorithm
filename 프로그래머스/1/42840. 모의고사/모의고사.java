4fcbeefd2e9efdf9d77d232e9a6f1981e862554d
Son JunHyung <109019081+sonjh919@users.noreply.github.com>
2025-01-20 16:24:55 +0900

    Update 모의고사.java

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -3,43 +3,29 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] answers) {
         List<Integer> answer = new ArrayList<>();
-        int[] a = {1,2,3,4,5}; //5
-        int[] b = {2,1,2,3,2,4,2,5}; //8
-        int[] c = {3,3,1,1,2,2,4,4,5,5}; //10
-        int aCount = 0;
-        int bCount = 0;
-        int cCount = 0;
         
+        int[][] pattern = {
+            {1,2,3,4,5},
+            {2,1,2,3,2,4,2,5},
+            {3,3,1,1,2,2,4,4,5,5}
+        };
+
+        int[] score = new int[3];
+
         for(int i=0;i<answers.length;i++){
-            int tmp = answers[i];
-            if(a[i%5]==tmp)aCount++;
-            if(b[i%8]==tmp)bCount++;
-            if(c[i%10]==tmp)cCount++;  
-        }
-        
-        if(aCount==bCount && bCount==cCount){
-            answer.add(1);
-            answer.add(2);
-            answer.add(3);
-            return answer.stream().mapToInt(Integer::intValue).toArray();
-        }
-        
-        if(aCount>=bCount){
-            if(aCount>=cCount){
-                answer.add(1);
+            for(int j=0;j<pattern.length;j++){
+                if(answers[i] == pattern[j][i%pattern[j].length]){
+                    score[j]++;
+                }
             }
         }
+
+        int max = Arrays.stream(score)
+            .max()
+            .getAsInt();
         
-        if(bCount>=aCount){
-            if(bCount>=cCount){
-                answer.add(2);
-            }
-        }
-        
-        if(cCount>=aCount){
-            if(cCount>=bCount){
-                answer.add(3);
-            }
+        for(int i=0;i<score.length;i++){
+            if(score[i]==max)answer.add(i+1);
         }
         
         return answer.stream().mapToInt(Integer::intValue).toArray();
