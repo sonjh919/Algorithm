e61d45e58ffc992ed24277392e520c24a7c45461
Son JunHyung <109019081+sonjh919@users.noreply.github.com>
2025-01-20 17:49:18 +0900

    [level 1] Title: 실패율, Time: 23.17 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,14 +1,15 @@
 import java.util.*;
 class Solution {
     public int[] solution(int N, int[] stages) {
+        // 도전자 수 분석 : O(N + M)  
         int[] challenger = new int[N+2];
-        for(int i=0;i<stages.length;i++){
+        for(int i=0;i<stages.length;i++){ // M
             challenger[stages[i]]++;
         }
         
         Map<Integer, Double> fail = new HashMap<>();
         double total = stages.length;
-        
+        // 실패율 구하기 : O(N)
         for(int i=1;i<=N;i++){
             if(challenger[i]==0)fail.put(i,0.);
             else{
@@ -17,6 +18,7 @@ class Solution {
             }
         }
         
+        // 정렬하기 : O(NlogN)
         return fail.entrySet().stream()
                 .sorted(((o1, o2) -> Double.compare(o2.getValue(), o1.getValue())))
                 .mapToInt(Map.Entry::getKey)
