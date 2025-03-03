ef88b6d68f65d523cec8f003cd19dc8aaaca83d9
JunHyung Son <kingjh1125@gmail.com>
2025-03-03 19:40:22 +0900

    [Bronze V] Title: 스타워즈 로고, Time: 96 ms, Memory: 13876 KB -BaekjoonHub

---
+++ 백준/Bronze/9653. 스타워즈 로고/스타워즈 로고.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // input
+//        String str = br.readLine();
+        // output
+        System.out.println("""
+                    8888888888  888    88888
+                   88     88   88 88   88  88
+                    8888  88  88   88  88888
+                       88 88 888888888 88   88
+                88888888  88 88     88 88    888888
+                
+                88  88  88   888    88888    888888
+                88  88  88  88 88   88  88  88
+                88 8888 88 88   88  88888    8888
+                 888  888 888888888 88  88      88
+                  88  88  88     88 88   88888888
+                """);
+//        bw.write();
+//        br.close();
+//        bw.close();
+    }
+}
