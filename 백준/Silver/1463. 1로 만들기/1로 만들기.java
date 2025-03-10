68ffb6726bd01268095ee3fd0e3ef991da7cb3fb
JunHyung Son <kingjh1125@gmail.com>
2025-03-10 21:54:07 +0900

    [Silver III] Title: 1로 만들기, Time: 228 ms, Memory: 76192 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    static Integer[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // input
+        int n = Integer.parseInt(br.readLine());
+
+        dp = new Integer[n+1];
+
+        dp[0] = dp[1] = 0;
+
+        bw.write(recur(n) + "");
+
+//      output
+        br.close();
+        bw.close();
+    }
+
+    private static int recur(int n) {
+        if(dp[n] == null) {
+            if (n % 6 == 0) {
+                dp[n] = Math.min(recur(n - 1), Math.min(recur(n / 3), recur(n / 2))) + 1;
+            } else if (n % 3 == 0) {
+                dp[n] = Math.min(recur(n / 3), recur(n - 1)) + 1;
+            } else if (n % 2 == 0) {
+                dp[n] = Math.min(recur(n / 2), recur(n - 1)) + 1;
+            } else {
+                dp[n] = recur(n - 1) + 1;
+            }
+        }
+
+        return dp[n];
+    }
+}
