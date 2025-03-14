cf0b97b51d5f53b510092e8aa87794d939e7e6e2
JunHyung Son <kingjh1125@gmail.com>
2025-03-15 08:37:27 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 112 ms, Memory: 14444 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    static Integer[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        dp = new Integer[n+1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]*2)%10007;
+        }
+
+        bw.write(dp[n] + "");
+
+        br.close();
+        bw.close();
+    }
+
+}
