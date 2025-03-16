fca0f975629b0aa6068d4533484b9ddcd4b72e9d
JunHyung Son <kingjh1125@gmail.com>
2025-03-16 09:28:33 +0900

    [Bronze II] Title: 피보나치 수, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    static Integer[] dp = new Integer[46];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        bw.write(dp[n-1] + "");
+
+        br.close();
+        bw.close();
+    }
+
+}
