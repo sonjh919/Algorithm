d5a17092e9aaeb662477fc61db02a44ca05090be
JunHyung Son <kingjh1125@gmail.com>
2025-03-18 09:43:37 +0900

    [Bronze V] Title: 등록, Time: 96 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // input
+bw.write("236\n"
+        + "kingjh1125");
+
+        // output
+        br.close();
+        bw.close();
+    }
+
+
+}
