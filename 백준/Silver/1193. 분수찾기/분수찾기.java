90615bb7b030b82daecb9e66320eaf0a89c5ba2e
Link <kingjh1125@gmail.com>
2025-07-28 12:03:15 +0900

    [Silver V] Title: 분수찾기, Time: 148 ms, Memory: 16032 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.java
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -1,36 +1,43 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static int X,N=1;
-
     public static void main(String[] args) throws IOException {
-        //given
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        //when
-        X = Integer.parseInt(br.readLine());
+        StringTokenizer st;
 
-        int i=1;
-        while(X>N){
-            N = N+i+1;
-            i++;
+        int X = Integer.parseInt(br.readLine());
+
+        int a = 0;
+        int b = 1;
+
+        for (int i = 1; i <= X; i++) {
+            if((a+b) % 2==0){
+                a--;
+                b++;
+            }else{
+                a++;
+                b--;
+            }
+
+
+            if(a==0)a++;
+            if(b==0)b++;
+
         }
 
-        int A = i, B=1;
-        for (int j = 0; j < N-X; j++) {
-            A--;
-            B++;
-        }
+        bw.write(a + "/" + b);
 
-        if(i%2==0) bw.write(A + "/" + B);
-        else bw.write(B + "/" + A);
-
-        //then
+        // output
         bw.flush();
-        bw.close();
         br.close();
+        bw.close();
     }
-
 }
+
