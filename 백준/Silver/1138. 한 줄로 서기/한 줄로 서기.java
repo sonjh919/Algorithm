6b9430cc2a154f6cbbea514d6d1dc4800546313b
Link <kingjh1125@gmail.com>
2025-08-03 12:29:07 +0900

    [Silver II] Title: 한 줄로 서기, Time: 108 ms, Memory: 14392 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -3,36 +3,42 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
 
+/*
+시간제한 2초: 최대 계산 횟수는 2억번
+N은 10보다 작거나 같은 자연수. -> 시간 복잡도는 신경 안써도 될 듯?
+ */
 public class Main {
 
-        public static void main(String[] args) throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-            StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
 
-            // input
-            int N = Integer.parseInt(br.readLine());
-            int[] tall = new int[N+1];
+        // input
+        int N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
 
-            st = new StringTokenizer(br.readLine());
-            for (int i = 1; i < N+1; i++) {
-                tall[i] = Integer.parseInt(st.nextToken());
-            }
+        // 1. arr 세팅
+        int[] arr = new int[N + 1];
+        for (int i = 1; i < N + 1; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-            List<Integer> line = new ArrayList<>();
-            for (int i = N; i >=1; i--) {
-                line.add(tall[i], i);
-            }
-
-            for (final Integer i : line) {
-                bw.write(i+" ");
-            }
+        // 2. List로 넣기
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int i = arr.length - 1; i > 0; i--) {
+            list.add(arr[i], i);
+        }
 
         // output
+        for (final Integer i : list) {
+            bw.write(i + " ");
+        }
+
+        // close
         bw.flush();
         br.close();
         bw.close();
