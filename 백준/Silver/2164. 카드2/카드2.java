cc55191185562bf14a32b37d29d1769e3205c5a9
Link <kingjh1125@gmail.com>
2025-08-13 08:58:24 +0900

    [Silver IV] Title: 카드2, Time: 204 ms, Memory: 29724 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -6,29 +6,36 @@ import java.io.OutputStreamWriter;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
-public class Main {
+/*
+시간제한 2초: 최대 계산 횟수는 2억번
 
+1 <= N <= 500,000
+
+시간 복잡도 : O(N) 가능, O(N^2) 불가능
+*/
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
 
-        Deque<Integer> queue = new ArrayDeque<>();
-        for (int i = N; i >= 1; i--) {
-            queue.add(i);
+        // input
+        int n = Integer.parseInt(br.readLine());
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        for (int i = n; i >= 1; i--) {
+            deque.add(i);
         }
 
-        while(queue.size()>1){
-            queue.removeLast();
-            queue.addFirst(queue.removeLast());
+        while (deque.size() > 1) { // O(N)
+            deque.removeLast();
+            deque.addFirst(deque.removeLast());
         }
 
-        bw.write(String.valueOf(queue.peek()));
+        bw.write(deque.peek() + "");
 
-
+        // close
         bw.flush();
         bw.close();
         br.close();
     }
-
 }
