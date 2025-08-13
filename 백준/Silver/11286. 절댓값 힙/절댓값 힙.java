a2add83e621f9d338902fe804e1cd02cc1a58fee
Link <kingjh1125@gmail.com>
2025-08-13 09:30:01 +0900

    [Silver I] Title: 절댓값 힙, Time: 336 ms, Memory: 26664 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -6,37 +6,43 @@ import java.io.OutputStreamWriter;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
-public class Main {
+/*
+시간제한 2초: 최대 계산 횟수는 2억번
 
+1 <= N <= 100,000
+
+시간 복잡도 : O(N) 가능, O(N^2) 불가능
+*/
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                if(Math.abs(o1) == Math.abs(o2))
-                    return o1 - o2;
-                else
-                    return Math.abs(o1) - Math.abs(o2);
-            }
-        });
+        // input
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> queue = new PriorityQueue<>(
+                Comparator.comparingInt((Integer x) -> Math.abs(x))
+                        .thenComparingInt(x -> x)
+        );
+        
+        int input;
+        for (int i = 0; i < n; i++) {
+            input = Integer.parseInt(br.readLine());
 
-        int N = Integer.parseInt(br.readLine());
-        int x;
-        for (int i = 0; i < N; i++) {
-            x = Integer.parseInt(br.readLine());
-            if(x==0){
-                bw.write(priorityQueue.isEmpty()?"0\n":priorityQueue.poll() + "\n");
+            if(input==0){
+                if(queue.isEmpty()){
+                    bw.write("0\n");
+                }
+                else bw.write(queue.poll()+"\n");
             }
-            else{
-                priorityQueue.add(x);
+            else {
+                queue.add(input);
             }
         }
 
+        // close
         bw.flush();
         bw.close();
         br.close();
     }
-
 }
