ca95d16876e89b07649bd636c2c45086ac7514d5
Link <kingjh1125@gmail.com>
2025-08-15 01:12:05 +0900

    [Bronze II] Title: 수 정렬하기, Time: 144 ms, Memory: 16480 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -3,35 +3,44 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
+/*
+시간제한 1초: 최대 계산 횟수는 1억번
+
+N <= 1,000
+
+시간 복잡도 : O(N^2) 까지 가능
+*/
 public class Main {
-
     public static void main(String[] args) throws IOException {
-        //given
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
-        List<Integer> list = new ArrayList<>();
+        // input
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
 
-        for (int i = 0; i < N; i++) {
-            list.add(Integer.parseInt(br.readLine()));
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
-        //when
-        Collections.sort(list);
-
-        for (Integer i : list) {
-            bw.write(i + "\n");
+        int temp;
+        for (int i = 0; i < n; i++) {
+            for(int j=i+1;j<n;j++){
+                if(arr[i] > arr[j]){
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
         }
 
-        //then
+        for(Integer i : arr){
+            bw.write(i+"\n");
+        }
+        // close
         bw.flush();
         bw.close();
         br.close();
     }
-
 }
