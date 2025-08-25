adfe74ac52204cb6a0a1c1a04587e486b92352b5
Link <kingjh1125@gmail.com>
2025-08-25 16:43:30 +0900

    [Silver V] Title: K번째 수, Time: 1952 ms, Memory: 376060 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/*
+시간제한 2초: 최대 계산 횟수는 2억번
+N <= 1,000
+O(N^2)
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        bw.write(arr[k-1]+"");
+
+        // close
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
+
