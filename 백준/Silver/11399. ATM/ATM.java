b9152805d6342182029db55e58ccb3350dc23255
Link <kingjh1125@gmail.com>
2025-08-25 15:44:27 +0900

    [Silver IV] Title: ATM, Time: 120 ms, Memory: 14520 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -3,40 +3,50 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/*
+시간제한 1초: 최대 계산 횟수는 1억번
+N <= 1,000
+O(N^2)
+ */
 public class Main {
-
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
 
-        int N = Integer.parseInt(br.readLine());
-        List<Integer> P = new ArrayList<>();
+        // 음 그냥 정렬하면 되는거 아닌가
+        int n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            P.add(Integer.parseInt(st.nextToken()));
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        Collections.sort(P);
-        int time = 0;
-        int sum = 0;
+        Arrays.sort(arr); // O(nlogn)
 
-        for(Integer i : P){
-            time += i;
-            sum += time;
+        // 깨알 합배열 ㅎ
+        int[] sum = new int[n];
+        sum[0] = arr[0];
+        for (int i = 1; i < sum.length; i++) {
+            sum[i] = arr[i] + sum[i-1];
         }
 
-        bw.write(sum+"");
+        int answer = 0;
+        for (int i : sum) {
+            answer += i;
+        }
 
+        bw.write(answer+"");
+
+        // close
         bw.flush();
-        bw.close();
         br.close();
+        bw.close();
     }
-
 }
+
