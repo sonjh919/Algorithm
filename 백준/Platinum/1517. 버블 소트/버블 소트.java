0a037fa829ab3c391f53897545497529be6380c5
Link <kingjh1125@gmail.com>
2025-08-27 10:10:30 +0900

    [Platinum V] Title: 버블 소트, Time: 544 ms, Memory: 72564 KB -BaekjoonHub

--- 백준/Platinum/1517. 버블 소트/버블 소트.java
+++ 백준/Platinum/1517. 버블 소트/버블 소트.java
@@ -15,7 +15,7 @@ N <= 500,000
 public class Main {
 
     public static int[] arr, tmp;
-    public static long result;
+    public static long result; // N<= 500,000일 때, 최대 스왑 횟수는 NC2이므로 int 범위 초과 -> long!!
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -33,7 +33,6 @@ public class Main {
         }
 
         result = 0;
-
         merge_sort(1,n);
 
         bw.write(result+"");
@@ -45,43 +44,44 @@ public class Main {
     }
 
     private static void merge_sort(int start, int end) {
-        if (end - start < 1) return;
-        int middle = start + (end-start)/2;
+        if (end - start < 1) return; // start==end, 즉 원소가 하나일 때 리턴
+        int middle = start + (end-start)/2; // 가운데 세팅
 
-        merge_sort(start,middle);
+        merge_sort(start,middle); // 분할 Divide
         merge_sort(middle+1,end);
 
-        for(int i = start;i<=end;i++){
+        for(int i = start;i<=end;i++){ // 임시 배열 할당
             tmp[i] = arr[i];
         }
 
-        int k=start;
-        int index1 = start;
-        int index2 = middle + 1;
+        // 정복 Conquer
+        int current_index = start; // 정렬되는 인덱스는 start부터 시작
+        int index1_pointer = start; // 투포인터1 : 앞쪽 배열 시작점
+        int index2_pointer = middle + 1; // 투포인터2 : 뒤쪽 배열 시작점
 
-        while(index1 <= middle && index2 <= end){
-            if(tmp[index1] > tmp[index2]){
-                arr[k] = tmp[index2];
-                result = result + index2 - k;
-                k++;
-                index2++;
+        while(index1_pointer <= middle && index2_pointer <= end){ // 두 배열 중 어느 한 쪽이 끝에 갈 때까지
+            if(tmp[index1_pointer] > tmp[index2_pointer]){ // 앞쪽 배열이 뒤쪽 배열보다 크다면
+                arr[current_index] = tmp[index2_pointer]; // 뒤쪽 배열의 포인터가 가리키는 값을 arr에 정렬
+                result = result + index2_pointer - current_index; // 인덱스 이동만큼 result에 더해주기!!(==swap 횟수!)
+                current_index++;
+                index2_pointer++;
             }
-            else{
-                arr[k] = tmp[index1];
-                k++;
-                index1++;
+            else{ // 앞쪽 배열이 뒤쪽 배열보다 작다면
+                arr[current_index] = tmp[index1_pointer]; // 앞쪽 배열의 포인터가 가리키는 값을 arr에 정렬
+                current_index++;
+                index1_pointer++;
             }
         }
 
-        while(index1 <= middle){
-            arr[k] = tmp[index1];
-            k++;
-            index1++;
+        while(index1_pointer <= middle){ // 뒤쪽 배열 정렬이 끝났을 경우 앞쪽 배열은 이미 정리되어 있는 것. 그대로 추가해주기.
+            arr[current_index] = tmp[index1_pointer];
+            current_index++;
+            index1_pointer++;
         }
-        while(index2 <= end){
-            arr[k] = tmp[index2];
-            k++;
-            index2++;
+        while(index2_pointer <= end){ // 마찬가지로, 뒤쪽 배열 그대로 추가해주기.
+            arr[current_index] = tmp[index2_pointer];
+            current_index++;
+            index2_pointer++;
         }
     }
 }
