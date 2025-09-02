f69bf950d754cdb2dc668122604ac71fbd093a61
Link <kingjh1125@gmail.com>
2025-09-03 00:01:46 +0900

    [Silver IV] Title: 수 찾기, Time: 600 ms, Memory: 48088 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,46 +1,62 @@
-import static java.util.stream.Collectors.toList;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.StringTokenizer;
 
+/*
+시간제한 1초: 최대 계산 횟수는 1억번
+ N(1 ≤ N ≤ 100,000)
+ M(1 ≤ M ≤ 100,000)
+O(N) 가능, O(NlogN), O(NlogM) 가능, O(N*M) 불가능
+*/
 public class Main {
+    static int[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
 
-        br.readLine();
-        List<Integer> A;
+        // input
+        int N = Integer.parseInt(br.readLine());
+        arr = new int[N];
 
-        String[] input = br.readLine().split(" ");
-        A = Arrays.stream(input)
-                .map(Integer::parseInt)
-                .collect(toList());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
 
-        Collections.sort(A);
-
-        br.readLine();
-
-        input = br.readLine().split(" ");
-
-        for (String s : input) {
-            bw.write(checkIsContain(Integer.parseInt(s), A) + "\n");
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            bw.write(search(Integer.parseInt(st.nextToken()))+"\n");
         }
 
+        // close
         bw.flush();
         bw.close();
         br.close();
     }
 
-    private static int checkIsContain(int i, List<Integer> A) {
-        int tmp = Collections.binarySearch(A, i);
-        if(tmp<0)return 0;
-        return 1;
+    static int search(int num){
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start<=end){
+            int middle = (start+end)/2;
+            if(num==arr[middle])return 1;
+            else if(num<arr[middle]){
+                end = middle-1;
+            }
+            else{
+                start = middle+1;
+            }
+        }
+        return 0;
     }
+
 }
