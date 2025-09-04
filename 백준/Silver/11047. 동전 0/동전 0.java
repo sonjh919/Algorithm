770c253c8b21be197ec24e9f92cf93c4df1c6e4a
Link <kingjh1125@gmail.com>
2025-09-05 08:38:12 +0900

    [Silver IV] Title: 동전 0, Time: 108 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -3,40 +3,46 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
+/*
+시간제한 1초: 최대 계산 횟수는 1억번
+
+(1 ≤ N ≤ 10, 1 ≤ K ≤ 100,000,000)
+
+O(K) 가능.
+*/
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
 
+        // input
+        st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        int sum = 0;
 
-        List<Integer> list = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
         for (int i = 0; i < N; i++) {
-            list.add(Integer.parseInt(br.readLine()));
+            stack.add(Integer.parseInt(br.readLine()));
         }
 
-        Collections.reverse(list);
-        for(Integer i : list){
-            sum += K/i;
-            K %= i;
+        int sum = 0;
+        while(!stack.empty()){
+            sum += K/stack.peek();
+            K %= stack.pop();
+
             if(K==0)break;
         }
 
         bw.write(sum+"");
 
-
+        // close
         bw.flush();
         bw.close();
         br.close();
     }
-
 }
