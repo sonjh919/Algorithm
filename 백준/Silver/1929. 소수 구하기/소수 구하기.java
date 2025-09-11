f5aa943caa49b752a8ff3d23e6c9ac7eb2446a57
Link <kingjh1125@gmail.com>
2025-09-11 09:11:07 +0900

    [Silver III] Title: 소수 구하기, Time: 480 ms, Memory: 20740 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -3,28 +3,41 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.math.BigInteger;
 import java.util.StringTokenizer;
 
+/*
+시간제한 2초: 최대 계산 횟수는 2억번
+(1 ≤ M ≤ N ≤ 1,000,000) -> 다돌면 O(N^2), sqrt()로 돌면 됨!
+*/
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        // input
+        st = new StringTokenizer(br.readLine());
         int M = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
 
-        BigInteger num;
         for (int i = M; i <= N; i++) {
-            num = new BigInteger(String.valueOf(i));
-            if(num.isProbablePrime(10))bw.write(num+"\n");
+            if(isPrime(i)){
+                bw.write(i+"\n");
+            }
         }
 
+        // close
         bw.flush();
         bw.close();
         br.close();
     }
 
+    private static boolean isPrime(final int m) {
+        if(m==1) return false;
+        for (int i = 2; i <= Math.sqrt(m); i++) {
+            if(m%i==0)return false;
+        }
+        return true;
+    }
 }
