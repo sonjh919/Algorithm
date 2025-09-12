a471da470c0d55d04569aae3f049420573f890eb
Link <kingjh1125@gmail.com>
2025-09-12 09:23:47 +0900

    [Bronze I] Title: 최소공배수, Time: 152 ms, Memory: 16792 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.java
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -6,6 +6,10 @@ import java.io.OutputStreamWriter;
 import java.math.BigInteger;
 import java.util.StringTokenizer;
 
+/*
+시간제한 1초: 최대 계산 횟수는 1억번
+ T(1 ≤ T ≤ 1,000)
+*/
 public class Main {
 
     public static void main(String[] args) throws IOException {
@@ -13,21 +17,20 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
-        int T = Integer.parseInt(br.readLine());
-        int A, B, lcm;
-
-        for (int i = 0; i < T; i++) {
+        // input
+        int N = Integer.parseInt(br.readLine());
+        int A;
+        int B;
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             A = Integer.parseInt(st.nextToken());
             B = Integer.parseInt(st.nextToken());
-
-            lcm = (A * B) / (BigInteger.valueOf(A).gcd(BigInteger.valueOf(B))).intValue();
-            bw.write(lcm+"\n");
+            bw.write((A*B) / BigInteger.valueOf(A).gcd(BigInteger.valueOf(B)).intValue()+"\n");
         }
-        
+
+        // close
         bw.flush();
         bw.close();
         br.close();
     }
-
 }
