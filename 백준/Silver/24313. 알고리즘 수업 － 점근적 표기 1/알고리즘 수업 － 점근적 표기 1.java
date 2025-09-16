b3bd1bbd569d1d7506af8a69b54c306991c9688a
Link <kingjh1125@gmail.com>
2025-09-16 20:37:08 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 100 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.java
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.java
@@ -1,33 +1,27 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int a1, a0, c, n0;
-    static int n;
-
     public static void main(String[] args) throws IOException {
-        //given
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        //when
-        a1 = Integer.parseInt(st.nextToken());
-        a0 = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(br.readLine());
-        n0 = Integer.parseInt(br.readLine());
+        int a1 = Integer.parseInt(st.nextToken());
+        int a0 = Integer.parseInt(st.nextToken());
 
-        if(a1*n0+a0<=c*n0 && a1<=c) bw.write(String.valueOf(1));
-        else bw.write(String.valueOf(0));
+        int c = Integer.parseInt(br.readLine());
+        int n0 = Integer.parseInt(br.readLine());
+        br.close();
 
-        //then
+        if (a1*n0 + a0 <= c*n0 && c >= a1) {
+            bw.write("1");
+        } else {
+            bw.write("0");
+        }
+
         bw.flush();
         bw.close();
-        br.close();
-    }
 
+    }
 }
