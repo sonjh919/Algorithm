27817b57356e401ac81c154df0e7eec9e20e6a98
Link <kingjh1125@gmail.com>
2025-09-17 08:48:31 +0900

    [Bronze I] Title: 이항 계수 1, Time: 104 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -5,28 +5,40 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
+/*
+시간제한 1초: 최대 계산 횟수는 1억번
+*/
 public class Main {
-
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        // input
+        st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        bw.write(factorial(N)/(factorial(K)*factorial(N-K))+"");
+        int[][] dp = new int[N+1][N+1];
 
+        for (int i = 0; i <= N; i++) {
+            dp[i][1] = i;
+            dp[i][0] = 1;
+            dp[i][i] = 1;
+        }
 
+        for (int i = 2; i <= N; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i-1][j-1];
+            }
+        }
+
+        bw.write(dp[N][K]+"");
+
+        // close
         bw.flush();
         bw.close();
         br.close();
     }
-
-    private static int factorial(int n) {
-        if(n==0)return 1;
-        return n*factorial(n-1);
-    }
-
 }
