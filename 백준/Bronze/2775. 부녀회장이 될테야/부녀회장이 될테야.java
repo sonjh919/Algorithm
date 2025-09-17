6319d658b8f53ee1880a17d385b2d87255688e9f
Link <kingjh1125@gmail.com>
2025-09-18 08:56:59 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 120 ms, Memory: 14468 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -4,32 +4,40 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
+/*
+시간제한 5초: 최대 계산 횟수는 0.5억번
+*/
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int[][] dp = new int[15][15];
+
+        for (int i = 0; i < 15; i++) {
+            dp[i][1] = 1;
+            dp[0][i] = i;
+        }
+
+        for (int i = 1; i < 15; i++) {
+            for (int j = 2; j < 15; j++) {
+                dp[i][j] = dp[i][j-1] + dp[i-1][j];
+            }
+        }
+
+        // input
         int T = Integer.parseInt(br.readLine());
-        for (int i = 0; i < T; i++) {
+        for (int t = 0; t < T; t++) {
             int K = Integer.parseInt(br.readLine());
             int N = Integer.parseInt(br.readLine());
-            int sum = sum(K,N);
-            bw.write(sum + "\n");
+            bw.write(dp[K][N]+"\n");
         }
-        
-        br.close();
-        bw.close();
-    }
 
-    private static int sum(int k, int n) {
-        int sum = 0;
-        if(k==0){
-            return n;
-        }
-        for (int i = 1; i <= n; i++) {
-            sum += sum(k-1, i);
-        }
-        return sum;
+
+        // close
+        bw.flush();
+        bw.close();
+        br.close();
     }
-    
 }
