00c1ec44606ce9030fa66ec7371f22ebecdb5a9f
Link <kingjh1125@gmail.com>
2025-09-19 08:41:46 +0900

    [Silver III] Title: 1로 만들기, Time: 132 ms, Memory: 18212 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -4,41 +4,37 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
+/*
+시간제한 0.15초: 최대 계산 횟수는 0.15억번(1500만)
+(1 <= N <= 100000)
+O(N) 가능! -> 다 만들어놔도 된다. -> dp
+*/
 public class Main {
 
-    static Integer[] dp;
+    static int N;
+    static int[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         // input
-        int n = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
 
-        dp = new Integer[n+1];
-
+        dp = new int[N+1];
         dp[0] = dp[1] = 0;
 
-        bw.write(recur(n) + "");
-
-//      output
-        br.close();
-        bw.close();
-    }
-
-    private static int recur(int n) {
-        if(dp[n] == null) {
-            if (n % 6 == 0) {
-                dp[n] = Math.min(recur(n - 1), Math.min(recur(n / 3), recur(n / 2))) + 1;
-            } else if (n % 3 == 0) {
-                dp[n] = Math.min(recur(n / 3), recur(n - 1)) + 1;
-            } else if (n % 2 == 0) {
-                dp[n] = Math.min(recur(n / 2), recur(n - 1)) + 1;
-            } else {
-                dp[n] = recur(n - 1) + 1;
-            }
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i-1]+1;
+            if(i%3==0)dp[i] = Math.min(dp[i], dp[i/3]+1);
+            if(i%2==0)dp[i] = Math.min(dp[i],dp[i/2]+1);
         }
 
-        return dp[n];
+        bw.write(dp[N] + "");
+
+        // close
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
