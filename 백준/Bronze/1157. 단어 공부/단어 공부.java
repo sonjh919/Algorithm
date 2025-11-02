7fac860e3107f5c014f97f598f1373b866ba986c
Link <kingjh1125@gmail.com>
2025-11-02 21:49:18 +0900

    [Bronze I] Title: 단어 공부, Time: 252 ms, Memory: 21216 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,42 +1,58 @@
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
+
 public class Main {
-    static String S;
-    static int[] list = new int[26];
-    static int max = 0;
-    static char s;
 
     public static void main(String[] args) throws IOException {
-        //given
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        S = br.readLine().toUpperCase();
+        int[] count = new int[26];
 
-        //when
-        for (int i = 0; i < S.length(); i++) {
-            list[S.charAt(i)-'A']++;
+        String s = br.readLine();
+        s = s.toUpperCase();
+
+        // count 대입
+        for(int i=0;i<s.length();i++){
+            count[s.charAt(i) - 'A']++;
         }
 
-        for (int i = 0; i < list.length; i++) {
-            if(max<list[i]){
-                max=list[i];
-                s = (char)(i+'A');
+
+
+        // 제일 큰거 찾기
+        int max = Integer.MIN_VALUE;
+        int alphabet = -1;
+
+        for (int i=0;i<count.length;i++){
+            if(count[i] > max) {
+                max = count[i];
+                alphabet = i;
             }
-            else if (max==list[i])
-                s = '?';
         }
 
-        bw.write(s);
+        int duplicate = 0;
+        for(int c: count){
+            if(max == c)duplicate++;
+        }
 
-        //then
+        if(duplicate>=2){
+            bw.write("?");
+        }
+        else{
+            bw.write(alphabet+'A');
+        }
+
+
+        // input
+
+        // close
         bw.flush();
         bw.close();
         br.close();
     }
-
 }
